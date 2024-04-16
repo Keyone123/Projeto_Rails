e6914dbe83fdcc311195675a52bcd07f42e7c868
Kauã Marques <mkaua1011@gmail.com>
2024-04-15 20:43:25 -0500

    Static Pages

--- app/controllers/pagina_estatica_controller.rb
+++ app/controllers/pagina_estatica_controller.rb
@@ -2,4 +2,19 @@ class PaginaEstaticaController < ApplicationController
   def index
     @nome = "Kauã"
   end
+
+  def cadastro
+  end
+
+  def visualizar
+  end
+
+  def contato
+  end
+
+  def termos
+  end
+
+  def privacidade
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,12 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   root to: "pagina_estatica#index"
 
+  get "contato", to: "pagina_estatica#contato"
+  get "cadastro", to: "pagina_estatica#cadastro"
+  get "visualizar", to: "pagina_estatica#visualizar"
+  get "termos", to: "pagina_estatica#termos"
+  get "privacidade", to: "pagina_estatica#privacidade"
+
   # Defines the root path route ("/")
   # root "posts#index"
 end
